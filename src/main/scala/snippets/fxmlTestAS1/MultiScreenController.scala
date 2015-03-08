57049f47889fc115673b472e3f6c93c38cfb647a
adrian <astachurski@gmail.com>
2015-03-08 21:36:48 +0100

    now, I have direct access access to controller. Still no way to use multiple controllers.

--- src/main/scala/snippets/fxmlTestAS1/MultiScreenController.scala
+++ src/main/scala/snippets/fxmlTestAS1/MultiScreenController.scala
@@ -1,20 +1,49 @@
 package snippets.fxmlTestAS1
 
 import scalafx.event.ActionEvent
+import scalafx.scene.input.MouseEvent
 import scalafx.scene.layout.{StackPane, Pane}
 import scalafxml.core.macros.sfxml
 
+trait MultiScreenControllerIntf {
+
+  def initialize()
+}
+
 @sfxml
 class MultiScreenController(
-                             private val pane1: Pane,
+                             private val paneExaminations: Pane,
                              private val pane2: Pane,
                              private val paneMain: Pane,
                              private val stackPainMain: StackPane,
-                             private val myService: MyService) {
+                             private val myService: MyService,
+                             private val paneEnterExaminationData: Pane) extends MultiScreenControllerIntf {
+
+
+  def btnShowEnterExaminationData(event: ActionEvent) = {
+
+    paneEnterExaminationData.setVisible(true)
+    paneMain.setVisible(false)
+    paneExaminations.setVisible(false)
+
+  }
+
+  def onBtnExitToPaneMainClicked(event: MouseEvent) = {
+
+    initialize()
+
+  }
+
+
+  def onBtnBypassExamDataClicked(event: MouseEvent) = {
+    paneExaminations.setVisible(true)
+    paneEnterExaminationData.setVisible(false)
+    paneMain.setVisible(false)
+  }
 
 
   def onBtnShowWindow1(event: ActionEvent) = {
-    pane1.setVisible(true)
+    paneExaminations.setVisible(true)
     paneMain.setVisible(false)
     myService.printSth()
 
@@ -28,7 +57,7 @@ class MultiScreenController(
 
   def onBtnGoBack1(event: ActionEvent) = {
     paneMain.setVisible(true)
-    pane1.setVisible(false)
+    paneExaminations.setVisible(false)
 
   }
 
@@ -38,6 +67,14 @@ class MultiScreenController(
 
   }
 
+  override def initialize(): Unit = {
+
+    paneExaminations.setVisible(false)
+    pane2.setVisible(false)
+    paneEnterExaminationData.setVisible(false)
+    paneMain.setVisible(true)
+    println("initialized")
 
 
+  }
 }
--- src/main/scala/snippets/fxmlTestAS1/MultiScreenTest.scala
+++ src/main/scala/snippets/fxmlTestAS1/MultiScreenTest.scala
@@ -1,43 +1,45 @@
 package snippets.fxmlTestAS1
 
+import java.net.URL
 import scalafx.application.JFXApp
 import scalafx.Includes._
 import scalafx.scene.Scene
 import scala.reflect.runtime.universe.typeOf
-import scalafx.scene.control.Button
-import scalafx.scene.paint.Color
-import scalafxml.core.{FXMLView, DependenciesByType}
+import scalafxml.core.DependenciesByType
+import scalafxml.core.FXMLLoader
+import javafx.{scene => jfxs}
 
-case class MyService(){
 
+case class MyService() {
   def printSth() = {
     println("from my service 1!!!")
-
   }
-
-
 }
 
 object MultiScreenTest extends JFXApp {
 
-  val res =  getClass.getResource("multiScreenTest.fxml")
+  val res: URL = getClass.getResource("multiScreenTest.fxml")
+  val deps: DependenciesByType = new DependenciesByType(Map(typeOf[MyService] -> new MyService()))
+  val loader = new FXMLLoader(res, deps)
+  loader.load()
 
-  val root = FXMLView(res,
-    new DependenciesByType(Map(
-      typeOf[MyService] -> new MyService())))
+  val root = loader.getRoot[jfxs.Parent]
 
   stage = new JFXApp.PrimaryStage() {
     title = "Hello world"
-    scene = new Scene(root){
+    scene = new Scene(root) {
       //fill = Color.Transparent
     }
   }
 
   val n = root.lookup("#btnBadania")
 
-  if (n != null){
-    n. asInstanceOf[javafx.scene.control.Button].text = "A pupu !"
+  if (n != null) {
+    n.asInstanceOf[javafx.scene.control.Button].text = "A pupu !"
   }
+
+  val ctrl = loader.getController[MultiScreenControllerIntf]()
+  ctrl.initialize()
 
 
   //stage.setFullScreen(true)
